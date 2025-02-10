3b067f776cb8c3705955fea9e52b8219c6ad976a
Kavu12 <kavusikan22@gmail.com>
2025-02-10 17:49:10 +0100

    changement de thisAmount par getcharge

--- r6a06_refactorisation_dirigee_etudiant-main/src/main/java/fr/r6a06/Customer.java
+++ r6a06_refactorisation_dirigee_etudiant-main/src/main/java/fr/r6a06/Customer.java
@@ -30,18 +30,15 @@ public class Customer {
 
         String result = "Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-            //determine amounts for each line
-            thisAmount = getCharge(each, thisAmount);
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
                     each.getDaysRented() > 1) frequentRenterPoints++;
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(getCharge(each, 0)) + "\n";
+            totalAmount += getCharge(each, 0);
         }
 
         //add footer lines
