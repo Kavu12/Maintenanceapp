2e391b7a00d1d49cb216de25ec4e0c7089973fc1
Kavu12 <kavusikan22@gmail.com>
2025-02-17 12:51:27 +0100

    etape 10

---
+++ r6a06_refactorisation_dirigee_etudiant-main/src/test/java/fr/r6a06/PriceBuilder.java
@@ -0,0 +1,19 @@
+package fr.r6a06;
+
+import java.util.Collections;
+import java.util.Vector;
+public class PriceBuilder {
+
+    private int priceCode;
+
+    public Price build() {
+        Price result = ;
+
+    }
+
+    public PriceBuilder withPriceCode(int PriceCode) {
+        this.priceCode = PriceCode;
+        return this;
+    }
+
+}
